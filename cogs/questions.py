ec9dff7f84e1bb9b080a03dd0f28a7f9d65b387c
Ahsan Mashruf <killcode57@gmail.com>
2023-12-21 16:46:07 -0600

    fix score parsing

--- cogs/questions.py
+++ cogs/questions.py
@@ -34,7 +34,7 @@ class QandA(commands.Cog):
         print(content)
             
         category = content["category"]
-        value = int(content["value"].replace('$', ''))
+        value = int(content["value"].replace('$', '').replace(',', ''))
         question = self.HTMLtoMarkdown(content["question"])
         answer = self.HTMLtoMarkdown(content["answer"])
 
